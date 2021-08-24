04c87faa4c72dae624164eec93ea5f57a1b12a8c
irvin <irvin373@gmail.com>
2021-08-24 15:51:03 -0400

    material ui

--- App.tsx
+++ App.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {Navigation} from 'react-native-navigation';
+import { COLOR, ThemeContext, getTheme, Button } from 'react-native-material-ui';
+// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+// // Initialize Apollo Client
+// const client = new ApolloClient({
+//   uri: 'https://beta.pokeapi.co/graphql/v1beta',
+//   cache: new InMemoryCache()
+// });
+
+
+
+// you can set your style right here, it'll be propagated to application
+const uiTheme = {
+  palette: {
+    primaryColor: COLOR.green500,
+  },
+  toolbar: {
+    container: {
+      height: 50,
+    },
+  },
+};
 
 const HomeScreen = props => {
   return (
-    <View style={styles.root}>
-      <Text>Home Screen</Text>
-    </View>
+    // <ApolloProvider client={client}>
+      <ThemeContext.Provider value={getTheme(uiTheme)}>
+        <View style={styles.root}>
+          <Text>Home Screen</Text>
+          <Button primary text="Primary" />
+          <Button accent text="Accent" />
+          <Button raised primary text="Primary" />
+          <Button disabled text="Disabled" />
+        </View>
+      </ThemeContext.Provider>
+    // </ApolloProvider>
   );
 };
 
